9cf942f23af8f041629228cbe4afa9c1ef69bd9a
Dave LeBlanc <dave.leblanc@gmail.com>
2021-01-30 17:35:10 -0400

    updates adding families and restrictions to base admin functions

--- fmp/menu/admin.py
+++ fmp/menu/admin.py
@@ -1,13 +1,26 @@
+import re
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
-from .models import Ingredient, Recipe, Menu
+from .models import Ingredient, Recipe, Menu, Family
 
+class CreatedModifiedAdmin(admin.ModelAdmin):
+    readonly_fields = [ 'created_by', 'modified_by' ]
+
+class OwnerAdmin(admin.ModelAdmin):
+    readonly_fields = [ 'owner' ]
+    
 class IngredientInlineAdmin(admin.TabularInline):
     model = Recipe.ingredients.through
     insert_after = 'cooking_method'
+    
+    def has_add_permission(self, request, obj):
+        if obj.created_by!=request.user:
+            return False
+        return super().has_add_permission(request,obj)
 
-class RecipeAdmin(admin.ModelAdmin):
+class RecipeAdmin(CreatedModifiedAdmin):
     fieldsets = (
         (None, {
          'fields': ('name','dish_type','cooking_method')
@@ -20,7 +33,32 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = (IngredientInlineAdmin,)
     change_form_template = 'admin/custom/change_form.html'
 
+    readonly_fields = ('recipeingredients',)
     class Media:
+        css = {
+            'all': (
+                'css/admin.css',
+            )
+        }
+class MenuAdmin(CreatedModifiedAdmin):
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            queryset = Menu.objects.all()
+        else:
+            try:
+                queryset = Menu.objects.filter(family__members__in=[request.user])
+            except:
+                queryset = Menu.objects.none()
+        return queryset
+
+class FamilyMembersInlineAdmin(admin.TabularInline):
+    model = Family.members.through
+    insert_after = 'name'
+
+class FamilyAdmin(OwnerAdmin):
+     inlines = (FamilyMembersInlineAdmin,)
+     change_form_template = 'admin/custom/change_form.html'
+     class Media:
         css = {
             'all': (
                 'css/admin.css',
@@ -28,5 +66,6 @@ class RecipeAdmin(admin.ModelAdmin):
         }
 
 admin.site.register(Ingredient)
-admin.site.register(Menu)
+admin.site.register(Menu,MenuAdmin)
 admin.site.register(Recipe,RecipeAdmin)
+admin.site.register(Family,FamilyAdmin)
--- fmp/menu/models.py
+++ fmp/menu/models.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django_currentuser.db.models import CurrentUserField
 from .lang import *
 from time import strftime
 # Create your models here.
+from django.contrib.auth import get_user_model
+class Family(models.Model):
+    name = models.CharField(max_length=255)
+    owner = CurrentUserField(related_name="family_creator")
+    members = models.ManyToManyField(get_user_model(),through='FamilyMembers')
+
+    def __str__(self) -> str:
+        return self.name
+    class Meta:
+        verbose_name_plural="Families"
+
+class FamilyMembers(models.Model):
+    family = models.ForeignKey(Family,on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    family_admin = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural="Family Members"
 
 class Ingredient(models.Model):
     ingredient = models.CharField(max_length=80)
@@ -15,6 +34,8 @@ class Recipe(models.Model):
     dish_type = models.CharField(max_length=2, choices=DISH_TYPE_CHOICES,default=MAIN_ENTREE)
     cooking_method = models.CharField(max_length=2, choices=COOKING_METHOD_CHOICES,blank=True,null=True)
     instructions = models.TextField(blank=True,null=True)
+    created_by = CurrentUserField(related_name="recipe_created_by")
+    modified_by = CurrentUserField(related_name="recipe_modified_by",on_update=True)
     
     def __str__(self) -> str:
         return self.name
@@ -26,11 +47,20 @@ class RecipeIngredients(models.Model):
     optional = models.BooleanField(default=False)
     alternatives = models.ManyToManyField(Ingredient,related_name="alternatives",blank=True,null=True)
     prep_instructions = models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self) -> str:
+        return ''
+
+    class Meta:
+        verbose_name_plural="Recipe Ingredients "
     
 class Menu(models.Model):
     recipe = models.ForeignKey(Recipe,on_delete=models.CASCADE)
     date = models.DateField()
     meal_time = models.CharField(max_length=2,choices=MEAL_TIME_OPTIONS,default=SUPPER)
+    family = models.ForeignKey(Family,on_delete=models.CASCADE,null=True)
+    created_by = CurrentUserField(related_name="menu_created_by")
+    modified_by = CurrentUserField(related_name="menu_modified_by",on_update=True)
 
     def __str__(self) -> str:
         return "%s %s" % (self.recipe,self.date.strftime("%a %B %d")) 
